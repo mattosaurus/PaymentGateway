a1188e738da845658c0762f25ce86380ddbe9784
mattosaurus <mattosaurus@outlook.com>
2019-08-15 10:10:25 +0100

    Add masking extensions

--- PaymentGateway/Extensions/StringExtensions.cs
+++ PaymentGateway/Extensions/StringExtensions.cs
@@ -13,7 +13,26 @@ namespace PaymentGateway.Extensions
 
         public static string Mask(this string source, int start, int maskLength, char maskCharacter)
         {
+            if (start > source.Length -1)
+            {
+                throw new ArgumentException("Start position is greater than string length");
+            }
 
+            if (maskLength > source.Length)
+            {
+                throw new ArgumentException("Mask length is greater than string length");
+            }
+
+            if (start + maskLength > source.Length)
+            {
+                throw new ArgumentException("Start position and mask length imply more characters than are present");
+            }
+
+            string mask = new string(maskCharacter, maskLength);
+            string unMaskStart = source.Substring(0, start);
+            string unMaskEnd = source.Substring(start + maskLength - 1, source.Length - maskLength);
+
+            return unMaskStart + mask + unMaskEnd;
         }
     }
 }
